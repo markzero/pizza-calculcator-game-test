9ca98d2914679cff4c4d9c7f3117fda277b6468e
Marko Jakic <jakicmeister@gmail.com>
2019-04-16 17:23:02 +0200

    init react, webpack, babel, tailwindcss

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/tailwind.css';
+
+ReactDOM.render(
+  <div className="text-center">Init</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
